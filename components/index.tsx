f3a40372a7d3f7b941982a7f4337da882efe88fc
ahab-latif-qbatch <ahab.latif@qbatch.com>
2023-07-04 15:53:47 +0500

    Car Showcase app

---
+++ components/index.tsx
@@ -0,0 +1,21 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import SearchManufacturer from "./SearchManufacturer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import CarDetails from "./CarDetails";
+import CarCard from "./CarCard";
+
+export  {
+  Hero,
+  CustomButton,
+  NavBar,
+  Footer,
+  SearchManufacturer,
+  SearchBar,
+  CustomFilter,
+  CarDetails,
+  CarCard
+}
